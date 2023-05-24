bd5811acf2b92b7875a117d6423e263aed7e0481
Brian Gerdes <styrbjorne@outlook.com>
2023-05-23 19:38:44 -0700

    Added expense item; extracted date into component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';  
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
